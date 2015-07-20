f8e4fc060ec5ac25b9815a973000c67c5ce49d86
QCmu <mu.qiaochu@gmail.com>
2015-07-20 15:33:47 -0400

    Add simple calculation
    real-time calculation based on 'chatroom example'

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var sum = 0;
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
+    sum += parseFloat(msg);
+    io.emit('sum message', sum.toString());
     io.emit('chat message', msg);
   });
 });
